1bfeb04df85ba74d7f071a4c9b7b1d74b77136a6
Azizbek Xotamaliyev <azizbekxotamaliyev@gmail.com>
2024-04-01 19:30:03 +0500

    add aboutpage and fix subscribe

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 
 import "./HomePage.scss"
+import Subscribe from '../../components/subscribe/Subscribe'
 
 class HomePage extends Component {
   render() {
     return (
       <div className='container'>
+        <Subscribe/>
       </div>
     )
   }
