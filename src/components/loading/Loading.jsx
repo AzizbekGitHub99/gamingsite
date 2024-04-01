6546f7f8447d84372046cf095bb287f99d8dbdb5
Azizbek Xotamaliyev <azizbekxotamaliyev@gmail.com>
2024-04-01 12:44:54 +0500

    loading

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 
+import "./Loading.scss"
+
 class Loading extends Component {
   render() {
     return (
-      <div>Loading</div>
+      <div className='container'>
+        <div className="loader"></div>
+      </div>
     )
   }
 }
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,14 +2,13 @@ import React, { Component } from 'react'
 
 import "./HomePage.scss"
 import Btn from '../../components/button/Btn'
+import Loading from '../../components/loading/Loading'
 
 class HomePage extends Component {
   render() {
     return (
       <div>
-        <Btn>
-          Get
-        </Btn>
+        <Loading/>
       </div>
     )
   }
