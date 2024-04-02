351575a14498e80a89d7ee09593b4dd9f127cadc
MuharramOzodboyeva <muharramozodboyeva598@gmail.com>
2024-04-02 16:25:29 +0500

    changed

--- src/pages/news/components/card/Card.jsx
+++ src/pages/news/components/card/Card.jsx
@@ -3,7 +3,16 @@ import CardsPage from "./CardsPage";
 import card1 from "../assets/allone.png";
 import card2 from "../assets/alltwoo.png";
 import line from "../assets/line.png";
+import li1 from "../assets/comunity.png";
+import li2 from "../assets/girl.png";
+import li3 from "../assets/great.png";
+import li4 from "../assets/office.png";
+import li5 from "../assets/plastation.png";
+import li6 from "../assets/play.png";
+import li7 from "../assets/plastation2.png";
+
 import "./Cards.scss";
+import LittleCard from "./LittleCard";
 export class Card extends Component {
   render() {
     return (
@@ -31,7 +40,57 @@ export class Card extends Component {
                 paragraph2="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
               />
             </div>
-            <div className="card__right"></div>
+            <div className="card__right">
+              <LittleCard
+                image={li1}
+                btn1="John smash"
+                btn2=".5min"
+                title="Lorem Ipsum is simply dummy text dummy text ? "
+                color="#FF8484"
+              />
+              <LittleCard
+                image={li2}
+                btn1="John smash"
+                btn2=".5min"
+                title="Lorem Ipsum is simply dummy text dummy text ? "
+                color="#2351F5"
+              />
+              <LittleCard
+                image={li3}
+                btn1="John smash"
+                btn2=".5min"
+                title="Lorem Ipsum is simply dummy text dummy text ? "
+                color="#FF7C32"
+              />
+              <LittleCard
+                image={li4}
+                btn1="John smash"
+                btn2=".5min"
+                title="Lorem Ipsum is simply dummy text dummy text ? "
+                color="#2351F5"
+              />
+              <LittleCard
+                image={li5}
+                btn1="John smash"
+                btn2=".5min"
+                title="Lorem Ipsum is simply dummy text dummy text ? "
+                color="#23A54F"
+              />
+              <LittleCard
+                image={li6}
+                btn1="John smash"
+                btn2=".5min"
+                title="Lorem Ipsum is simply dummy text dummy text ? "
+                color="#2351F5"
+              />
+              <LittleCard
+                image={li7}
+                btn1="John smash"
+                btn2=".5min"
+                title="Lorem Ipsum is simply dummy text dummy text ? "
+                color="#9921C3"
+              />
+            </div>
           </div>
         </div>
       </section>
