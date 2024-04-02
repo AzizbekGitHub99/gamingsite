1ab970c9162837c2a6580cb321ed5cf18f263cfc
Sakiynat10 <xoliyorvasilbek4@gmail.com>
2024-04-02 16:16:34 +0500

    final changes

--- src/pages/services/ServicesPage.jsx
+++ src/pages/services/ServicesPage.jsx
@@ -1,9 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+
+import Hero from './components/hero/hero'
+import Info from './components/info/info'
+import SubscribeServices from './components/subscribe/SubscribeService'
 
 class ServicesPage extends Component {
   render() {
     return (
-      <div>ServicesPage</div>
+      <Fragment>
+        <Hero />
+        <Info />
+        <SubscribeServices/>
+      </Fragment>
     )
   }
 }
