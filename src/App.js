6cef6f97d814bc5e470b0a0285723512489be452
Azizbek Xotamaliyev <azizbekxotamaliyev@gmail.com>
2024-04-02 18:10:52 +0500

    news

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
-            <Route path="/" element={<HomePage />} />
+            <Route index element={<HomePage />} />
             <Route path="about" element={<AboutPage />} />
             <Route path="services" element={<ServicesPage />} />
             <Route path="news" element={<NewsPage />} />
--- src/pages/news/components/hero/Hero.jsx
+++ src/pages/news/components/hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import arrow from "../assets/right-arrow.png";
 import search from "../assets/search.png";
-import "./Hero.scss";
+import "./hero.scss";
 class Hero extends Component {
   render() {
     return (
